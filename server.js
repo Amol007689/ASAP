ba54f9c1069d48c3b9e5e033a1a7b86aee100248
Amol007689 <dimpuamol15@gmail.com>
2025-03-03 10:36:46 +0530

    ASAP4

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-app.listen(port, () => { 
-    console.log(⁠ Server is running on port ${port} ⁠);
+app.listen(port, (req,res) => { 
+    console.log(`⁠ Server is running on port ${port}`);
 });
